d54631e61a6ef5e9df92ea5134370cd1a01cd3c3
munetomo <fishmans0120@gmail.com>
2020-09-21 16:14:48 +0900

    バリデーションエラーメッセージ、日本語の設定

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module EventInfo
     config.time_zone = "Tokyo"
     # DBに保存する時間も日本時間にする（アプリケーションサーバのタイムゾーンを変更）
     config.active_record.default_timezone = :local
-
+    # エラーメッセージに日本語が使われるようになるが、日本語の辞書を用意する（gem）
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
