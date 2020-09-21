f10dce9b194bb36683e28e178ee9606a098680ec
munetomo <fishmans0120@gmail.com>
2020-09-21 13:23:08 +0900

    OmniAuthの設定

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.user OmniAuth::Builder do
+  if Rails.env.development? || Rails.env.test?
+    provider :github, "00ed921f00dd1c3c8fee", "3ca45aae8214996335108913b816ab4adc7db365"
+  else
+    provider :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret],
+
+  end
+end
