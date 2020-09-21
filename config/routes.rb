74af30486c3fb3c6342aaf068c4c7e44eccc594d
munetomo <fishmans0120@gmail.com>
2020-09-21 15:28:38 +0900

    ログイン管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :events
+  # ルートパスの設定
   root "welcome#index"
+  # OmniAuthによる認証が成功した場合は"/auth/:provider/callback"がコールバック用のURLとして利用されるのでログイン用のアクションに紐付け
   get "/auth/:provider/callback" => "sessions#create"
+  
   delete "/logout" => "sessions#destroy"
 end
