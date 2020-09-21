cff0ac4a4a7aa98dfe7a3feb3122db310dceda50
munetomo <fishmans0120@gmail.com>
2020-09-21 14:11:32 +0900

    タイムゾーンを日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module EventInfo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Rails内のタイムゾーンを日本時間にする
+    config.time_zone = "Tokyo"
+    # DBに保存する時間も日本時間にする（アプリケーションサーバのタイムゾーンを変更）
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
