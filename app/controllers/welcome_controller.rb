8e88a8d91e48677e601342c959d6e9940cdec49e
munetomo <fishmans0120@gmail.com>
2020-09-22 10:51:16 +0900

    kaminariで一覧ページにページネーション実装

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
   def index
+    @events = Event.page(params[:page]).per(10).where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
